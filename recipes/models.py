26e3d98b4816c4b2847975f53b1a942dd4d46cb5
Jeff-Lowrey <jeff@jaral.org>
2025-10-28 22:06:10 -0500

    added preliminary model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField()
+    english_quantity = models.DecimalField()
+    metric_quantity = models.IntegerField()
+    ENGLISH_UNIT_CHOICES = [
+    ("tsp","teaspoon"),
+    ("tablespoon","tbsp"),
+    ("cup","cup")
+    ("gallon","gal."),
+    ("ounce","oz"),
+    ("fluid ounce","fl oz"),
+    ("pound","lb")
+    ]
+    METRIC_UNIT_CHOICES = [
+    ("gram", "g"),
+    ("kilogram", "kg"),
+    ("milliliter", "mL"),
+    ("liter", "L"),
+    ]
+    UNIT_CHOICES = ENGLISH_UNIT_CHOICES+METRIC_UNIT_CHOICES
+    unit_type = models.CharField(choices=UNIT_CHOICES)
+    
+
+    def unit_consistency(self):
+        pass

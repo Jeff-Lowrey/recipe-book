645dd6c225113fa693a781ff210df85055efd72e
Jeff-Lowrey <jeff@jaral.org>
2025-11-13 17:31:05 -0600

    Initial buildout of views and forms.

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,37 +1,36 @@
 from django import forms
-from .models import Recipe, Ingredient, Step
+
+from .models import Recipe
 
 
-class RecipeForm(forms.ModelForm):
+class RecipeForm(forms.ModelForm[Recipe]):
     """Form for creating and editing Recipe instances."""
 
     class Meta:
         model = Recipe
-        fields = ['name', 'style', 'flaver_profile', 'ingredients', 'steps']
+        fields = ["name", "style", "flaver_profile", "ingredients", "steps"]
         widgets = {
-            'name': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter recipe name'
-            }),
-            'style': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter recipe style'
-            }),
-            'flaver_profile': forms.CheckboxSelectMultiple(),
-            'ingredients': forms.CheckboxSelectMultiple(),
-            'steps': forms.CheckboxSelectMultiple(),
+            "name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Enter recipe name"}
+            ),
+            "style": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Enter recipe style"}
+            ),
+            "flaver_profile": forms.CheckboxSelectMultiple(),
+            "ingredients": forms.CheckboxSelectMultiple(),
+            "steps": forms.CheckboxSelectMultiple(),
         }
         labels = {
-            'name': 'Recipe Name',
-            'style': 'Cooking Style',
-            'flaver_profile': 'Flavor Profile',
-            'ingredients': 'Ingredients',
-            'steps': 'Steps',
+            "name": "Recipe Name",
+            "style": "Cooking Style",
+            "flaver_profile": "Flavor Profile",
+            "ingredients": "Ingredients",
+            "steps": "Steps",
         }
         help_texts = {
-            'name': 'Enter a descriptive name for your recipe',
-            'style': 'E.g., Italian, Chinese, Fusion, etc.',
-            'flaver_profile': 'Select all flavor profiles that apply',
-            'ingredients': 'Select the ingredients for this recipe',
-            'steps': 'Select the steps for this recipe',
+            "name": "Enter a descriptive name for your recipe",
+            "style": "E.g., Italian, Chinese, Fusion, etc.",
+            "flaver_profile": "Select all flavor profiles that apply",
+            "ingredients": "Select the ingredients for this recipe",
+            "steps": "Select the steps for this recipe",
         }
